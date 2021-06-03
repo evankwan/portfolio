fd30ce6e938a1ba5fe07bcf20e2fa6f5d1215830
Evan Kwan <evanyuekwan@gmail.com>
2021-06-03 00:01:34 -0400

    project button toggle and hamburger click state change

--- scripts/script.js
+++ scripts/script.js
@@ -11,12 +11,39 @@ app.selectors = () => {
   app.hamburgerContainer = document.getElementById('hamburgerContainer');
   app.middleLine = document.getElementById('middleLine');
   app.navMenu = document.getElementById('navMenu')
+  app.navLinks = document.querySelectorAll('.navListItemLink');
+  app.projectPreviews = document.querySelectorAll('.projectPreview');
 }
 
 app.eventListeners = () => {
-  hamburgerToggler.addEventListener('click', () => {
+  app.hamburgerToggler.addEventListener('click', () => {
     hamburger.classList.toggle('whiteHamburger');
     navMenu.classList.toggle('showMenu');
+  });
+
+  app.projectPreviews.forEach((project) => {
+    project.addEventListener('click', () => {
+      project.classList.toggle('projectFocused');
+    })
+    project.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        project.classList.toggle('projectFocused');
+      }
+    })
+  })
+
+  app.navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      hamburger.classList.toggle('whiteHamburger');
+      navMenu.classList.toggle('showMenu');
+    })
+
+    link.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        hamburger.classList.toggle('whiteHamburger');
+        navMenu.classList.toggle('showMenu');
+      }
+    })
   })
 }
 
